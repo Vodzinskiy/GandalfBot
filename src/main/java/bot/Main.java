7a91d789ff9d418e24564aad7ebc4d0f6aa5ecdf
Vodzinskiy <roma.vodzinskiy@gmail.com>
2023-03-02 19:58:24 +0200

    add basic telegram bot structure

--- src/main/java/bot/Main.java
+++
@@ -1,7 +0,0 @@
-package bot;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/vodzinskiy/GandalfBot.java
@@ -0,0 +1,30 @@
+package org.vodzinskiy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Slf4j
+@Component
+public class GandalfBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
